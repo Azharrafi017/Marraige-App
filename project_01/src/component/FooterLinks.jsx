b6afced3495755fde4276727fd1b3cafacf3bd78
Azahar <azaharrafi17@gmail.com>
2024-08-12 11:01:56 +0530

    second commit

---
+++ project_01/src/component/FooterLinks.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Links() {
+  return (
+    <div className="footer-link">
+      <div className="link-container font-style">
+        <span>Support Links</span>
+        <span className="arrow">{">"}</span>
+      </div>
+      <div className="link-container font-style">
+        <span>Quick Links</span>
+        <span className="arrow">{">"}</span>
+      </div>
+      <div className="link-container font-style">
+        <span>Social media Links</span>
+        <span className="arrow">{">"}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Links;
